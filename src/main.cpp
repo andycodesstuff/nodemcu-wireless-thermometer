293c51c52288033b8a4f83e73e49b18677f9f99a
andycodesstuff <andrybusa@gmail.com>
2021-10-27 22:30:24 +0200

    Broadcast timestamp and temperature reading as JSON

--- src/main.cpp
+++ src/main.cpp
@@ -4,22 +4,41 @@
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+#define NTP_ADDRESS "europe.pool.ntp.org"
+#define NTP_INTERVAL_MS 60 * 1000
+#define NTP_OFFSET_S 2 * 60 * 60
 
 Thermistor thermistor(A0, 1024);
 WiFiNetwork network(WIFI_SSID, WIFI_PASSPHRASE);
+WiFiUDP ntpUDP;
+NTPClient ntpClient(ntpUDP, NTP_ADDRESS, NTP_OFFSET_S, NTP_INTERVAL_MS);
 
 void setup() {
   Serial.begin(115200);
   delay(10);
 
   network.connect(30000);
+  ntpClient.begin();
 }
 
 void loop() {
-  double temp_C = thermistor.get_temperature_C();
-  network.send(String(temp_C).c_str());
+  ntpClient.update();
 
-  Serial.print("T: ");
-  Serial.println(temp_C);
+  // Get temperature reading and epoch timestamp
+  double temp_C = thermistor.get_temperature_C();
+  unsigned long epoch = ntpClient.getEpochTime();
+
+  // Construct JSON message
+  String msg = String("{ \"ts\": ") +
+               String(epoch) +
+               String(", \"t\": ") +
+               String(temp_C) +
+               String(" }");
+  network.send(msg.c_str());
+  Serial.println(msg);
+
   delay(1000);
 }
